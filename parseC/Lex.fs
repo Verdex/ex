cbd5ed3a6b241e63c1a38d1eafec48f513628e0e
David L. Inman <davidlevere@gmail.com>
2019-01-06 19:50:10 -0500

    int parsing

--- parseC/Lex.fs
+++ parseC/Lex.fs
@@ -6,6 +6,7 @@ module Lex =
 
     let mutable _text = ""
     let mutable _index = 0
+    let mutable _buffer = ""
 
 
     let _binOpChar = [| '.'
@@ -47,6 +48,52 @@ module Lex =
                || tryChar '\r'
                || tryChar '\t'
 
+    let tryComment = fun () -> 
+        if current () = '/' && next () = '/' then
+            _index <- _index + 2
+            true
+        else
+            false
+
+    let tryBlockComment = fun () -> 
+        if current () = '/' && next () = '*' then
+            _index <- _index + 2
+            true
+        else
+            false
+
+    let clearLine = fun () -> 
+        while not (endText ()) && current () <> '\n' && current () <> '\r' do
+            _index <- _index + 1
+        if not (endText ()) && (current () = '\n' || current () = '\r') then 
+            _index <- _index + 1
+
+    let clearBlockComment = fun () ->
+        let endComment = fun () -> current () = '*' && hasNext () && next () = '/'
+        while not (endText ()) && not (endComment()) do 
+            _index <- _index + 1
+        if not (endText ()) && (endComment ()) then
+            _index <- _index + 2
+    
+    let isDigit (c : char) = 
+        match c with
+        | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' -> true
+        | _ -> false
+
+    let tryInt = fun () ->
+        let mutable ds = "" 
+        let add (d:char) = ds <- ds + (string d)
+        if isDigit( current() ) then 
+            add (current ())
+            _index <- _index + 1
+            while not (endText ()) && isDigit( current () ) do 
+                add(current ())
+                _index <- _index + 1 
+            _buffer <- ds
+            true
+        else
+            false
+
     let public lex (input : string) =
         _text <- input
         _index <- 0
@@ -57,6 +104,10 @@ module Lex =
         while not (endText()) do
             if tryWhiteSpace () then
                 ()
+            elif tryComment () then
+                clearLine ()
+            elif tryBlockComment () then
+                clearBlockComment ()
             elif tryChar '(' then
                 add LParen 
             elif tryChar ')' then
@@ -69,6 +120,13 @@ module Lex =
                 add LSquare
             elif tryChar ']' then
                 add RSquare 
+            elif tryChar ',' then
+                add Comma 
+            elif tryChar ';' then
+                add SemiColon 
+            elif tryInt () then
+                add (Int (int _buffer))
+                _buffer <- ""
             else  
                 raise (Fatal "error")
 
--- parseC/Test.fs
+++ parseC/Test.fs
@@ -5,4 +5,12 @@ module Test =
     open Data
     open Lex
 
-    let main = printfn "%A" (lex " lah")
+    let main = 
+        printfn "%A" (lex "// blah ")
+        printfn "%A" (lex "/* blah */")
+        printfn "%A" (lex """
+        1209 
+        /* blah 
+            blah
+            blah
+        */""")
