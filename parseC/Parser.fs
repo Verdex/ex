3066a9b21a6caade7afa945a2ffa29f33492694d
David L. Inman <davidlevere@gmail.com>
2019-01-07 23:37:17 -0500

    inc prog parser

--- parseC/Parser.fs
+++ parseC/Parser.fs
@@ -4,12 +4,13 @@ module Parser =
 
     open Data
 
-    let mutable _text = [] 
+    let mutable _text : List<Token> = [] 
     let mutable _index = 0
-    let mutable _buffer = ""
+    let mutable _buffer : List<Token> = [] 
 
     let public parse (tokens : List<Token>) = 
         _text <- tokens
         _index <- 0
-        // _buffer <- ?
+        _buffer <- []
+
         ""
